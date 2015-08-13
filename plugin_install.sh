7e0c7e39cfb724fafc175a98beb4871a113eca1c
Rain Wang <wty21cn@163.com>
2015-08-13 21:46:13 +0100

    Add hexo-html-minifier plugin

--- plugin_install.sh
+++ plugin_install.sh
@@ -5,3 +5,5 @@ echo -e "\nfeed:\n  type: atom\n  path: atom.xml\n  limit: false\n" >> _config.y
 npm install hexo-generator-sitemap --save
 echo -e "\nsitemap:\n  path: sitemap.xml\n" >> _config.yml
 npm install hexo-deployer-git --save
+npm install hexo-html-minifier --save
+echo -e "\nhtml-minifier:\n  exclude:\n" >> _config.yml
